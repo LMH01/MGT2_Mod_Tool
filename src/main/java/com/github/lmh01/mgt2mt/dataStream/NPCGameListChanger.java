7bd0201c925ee67d2dfcfe54fa0a37a45e48ca3d
LordMZTE <lord@mzte.de>
2021-01-27 12:40:05 +0100

    fix logger crash and jar task creates fat jars

--- src/main/java/com/github/lmh01/mgt2mt/dataStream/NPCGameListChanger.java
+++ src/main/java/com/github/lmh01/mgt2mt/dataStream/NPCGameListChanger.java
@@ -1,7 +1,7 @@
 package com.github.lmh01.mgt2mt.dataStream;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
--- src/main/java/com/github/lmh01/mgt2mt/windows/WindowAddGenreToGames.java
+++ src/main/java/com/github/lmh01/mgt2mt/windows/WindowAddGenreToGames.java
@@ -1,8 +1,8 @@
 package com.github.lmh01.mgt2mt.windows;
 
 import com.github.lmh01.mgt2mt.dataStream.NPCGameListChanger;
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
