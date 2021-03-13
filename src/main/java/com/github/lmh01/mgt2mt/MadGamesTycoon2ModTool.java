421e6835ef8bb71fdfb7caf684df8f8b778ee3a8
Louis <louis.haensch01@gmail.com>
2021-03-13 17:38:06 +0100

    Changed version number to v1.7.0 for the upcomming release.

--- src/main/java/com/github/lmh01/mgt2mt/MadGamesTycoon2ModTool.java
+++ src/main/java/com/github/lmh01/mgt2mt/MadGamesTycoon2ModTool.java
@@ -13,9 +13,8 @@ import java.io.IOException;
 import java.util.Locale;
 
 public class MadGamesTycoon2ModTool {
-    //TODO Jede datei durchgehen und altes, ungenutzes Zeug weg löschen (z.B. Imports)
     private static final Logger LOGGER = LoggerFactory.getLogger(MadGamesTycoon2ModTool.class);
-    public static final String VERSION = "1.6.0";
+    public static final String VERSION = "1.7.0";
     public static void main(String[] args) throws IOException {
         ToolTipManager.sharedInstance().setDismissDelay(30000);
         ToolTipManager.sharedInstance().setInitialDelay(500);
