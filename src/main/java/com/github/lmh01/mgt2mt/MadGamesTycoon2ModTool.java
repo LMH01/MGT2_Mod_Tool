bc556c6545901d3a872512595f199d75d48d27f0
Louis <louis.haensch01@gmail.com>
2021-03-17 17:05:12 +0100

    Changed version number to 1.8.1 for upcoming release

--- src/main/java/com/github/lmh01/mgt2mt/MadGamesTycoon2ModTool.java
+++ src/main/java/com/github/lmh01/mgt2mt/MadGamesTycoon2ModTool.java
@@ -14,7 +14,7 @@ import java.util.Locale;
 
 public class MadGamesTycoon2ModTool {
     private static final Logger LOGGER = LoggerFactory.getLogger(MadGamesTycoon2ModTool.class);
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.8.1";
     public static void main(String[] args) throws IOException {
         ToolTipManager.sharedInstance().setDismissDelay(30000);
         ToolTipManager.sharedInstance().setInitialDelay(500);
