b3f5c51b5d0b658ee3d09100513f5081d0fe3db3
Louis <louis.haensch01@gmail.com>
2021-03-20 18:18:04 +0100

    When a new version is available the new key features are now displayed.

--- src/main/java/com/github/lmh01/mgt2mt/data_stream/UpdateChecker.java
+++ src/main/java/com/github/lmh01/mgt2mt/data_stream/UpdateChecker.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class UpdateChecker {
     public static String newestVersion = "";
+    public static String newestVersionKeyFeatures = "";
     private static final String UPDATE_URL = "https://www.dropbox.com/s/7dut2h3tqdc92xz/mgt2mtNewestVerion.txt?dl=1";
     private static final Logger LOGGER = LoggerFactory.getLogger(MadGamesTycoon2ModTool.class);
     public static void checkForUpdates(boolean showNoUpdateAvailableDialog){
@@ -20,10 +21,19 @@ public class UpdateChecker {
                     LOGGER.info("Checking for updates...");
                     java.net.URL url = new URL(UPDATE_URL);
                     Scanner scanner = new Scanner(url.openStream());
-                    newestVersion = scanner.nextLine();
+                    String currentLine = scanner.nextLine();
+                    newestVersion = currentLine;
                     if(!newestVersion.equals(MadGamesTycoon2ModTool.VERSION)){
                         LOGGER.info("New version found: " + newestVersion);
-                        if(JOptionPane.showConfirmDialog(null, "A new version is available: " + newestVersion + "\nIt is recommended to always use the newest version to keep this tool compatible with MGT2.\n\nDo you wan't to open the github repository to download the newest version?", "New version available", JOptionPane.YES_NO_OPTION) == 0){
+                        LOGGER.info("Key features:");
+                        StringBuilder stringBuilder = new StringBuilder();
+                        while(scanner.hasNextLine()){
+                            currentLine = scanner.nextLine();
+                            LOGGER.info(currentLine);
+                            stringBuilder.append(currentLine).append(System.getProperty("line.separator"));
+                        }
+                        newestVersionKeyFeatures = stringBuilder.toString();
+                        if(JOptionPane.showConfirmDialog(null, "A new version is available: " + newestVersion + "\nKey features:\n" + newestVersionKeyFeatures + "\nIt is recommended to always use the newest version to keep this tool compatible with MGT2.\n\nDo you wan't to open the github repository to download the newest version?", "New version available", JOptionPane.YES_NO_OPTION) == 0){
                             try {
                                 Utils.openGithubPage();
                             } catch (Exception e) {
