b9bd200741df8404f90ddf3f7f5d3de3cc585740
Louis <louis.haensch01@gmail.com>
2021-05-01 00:02:13 +0200

    Completed german translation. Every text that is displayed to the user should now be translatable. (fr #30)

--- src/main/java/com/github/lmh01/mgt2mt/mod/LicenceMod.java
+++ src/main/java/com/github/lmh01/mgt2mt/mod/LicenceMod.java
@@ -106,28 +106,28 @@ public class LicenceMod extends AbstractSimpleMod {
 
     @Override
     public void menuActionAddMod() {
-        JLabel labelAddLicence = new JLabel("Name: ");
+        JLabel labelAddLicence = new JLabel(I18n.INSTANCE.get("commonText.name") + ":");
         JTextField textFieldName = new JTextField();
 
         JPanel panelType = new JPanel();
-        JLabel labelType = new JLabel("Type: ");
+        JLabel labelType = new JLabel(I18n.INSTANCE.get("commonText.type") + ":");
         JComboBox comboBoxType = new JComboBox();
-        comboBoxType.setModel(new DefaultComboBoxModel<>(new String[]{"Movie", "Book", "Sport"}));
+        comboBoxType.setModel(new DefaultComboBoxModel<>(new String[]{I18n.INSTANCE.get("mod.licence.addMod.optionPaneMessage.movie"), I18n.INSTANCE.get("mod.licence.addMod.optionPaneMessage.book"), I18n.INSTANCE.get("mod.licence.addMod.optionPaneMessage.sport")}));
         panelType.add(labelType);
         panelType.add(comboBoxType);
 
         Object[] params = {labelAddLicence, textFieldName, panelType};
         while(true){
-            if(JOptionPane.showConfirmDialog(null, params, "Add Licence", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION){
+            if(JOptionPane.showConfirmDialog(null, params, I18n.INSTANCE.get("commonText.add.upperCase") + ": " + getType(), JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION){
                 if(!textFieldName.getText().isEmpty()){
                     StringBuilder newLicence = new StringBuilder();
                     newLicence.append(textFieldName.getText()).append(" ");
                     String selectedType = comboBoxType.getSelectedItem().toString();
-                    if(selectedType.equals("Movie")){
+                    if(selectedType.equals(I18n.INSTANCE.get("mod.licence.addMod.optionPaneMessage.movie"))){
                         newLicence.append("[MOVIE]");
-                    }else if(selectedType.equals("Book")){
+                    }else if(selectedType.equals(I18n.INSTANCE.get("mod.licence.addMod.optionPaneMessage.book"))){
                         newLicence.append("[BOOK]");
-                    }else if(selectedType.equals("Sport")){
+                    }else if(selectedType.equals(I18n.INSTANCE.get("mod.licence.addMod.optionPaneMessage.sport"))){
                         newLicence.append("[SPORT]");
                     }
                     boolean licenceAlreadyExists = false;
@@ -139,26 +139,26 @@ public class LicenceMod extends AbstractSimpleMod {
                     }
                     if(!licenceAlreadyExists){
                         StringBuilder stringBuilder = new StringBuilder();
-                        stringBuilder.append("Add this licence:").append(System.getProperty("line.separator"))
+                        stringBuilder.append(I18n.INSTANCE.get("mod.licence.addMod.confirm") + ":").append(System.getProperty("line.separator"))
                                 .append(textFieldName.getText()).append(System.getProperty("line.separator"))
                                 .append("Type: ").append(comboBoxType.getSelectedItem());
-                        if(JOptionPane.showConfirmDialog(null, stringBuilder.toString(), "Add this licence?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+                        if(JOptionPane.showConfirmDialog(null, stringBuilder.toString(), I18n.INSTANCE.get("commonText.add.upperCase") + ": " + getType(), JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
                             try {
                                 Backup.createBackup(getFile());
                                 ModManager.licenceMod.getEditor().addMod(newLicence.toString());
-                                TextAreaHelper.appendText(I18n.INSTANCE.get("textArea.added") + " " + I18n.INSTANCE.get("window.main.share.export.licence") + " - " + textFieldName.getText());
-                                JOptionPane.showMessageDialog(null, "Licence [" + textFieldName.getText() + "] has been added successfully!");
+                                TextAreaHelper.appendText(I18n.INSTANCE.get("textArea.added") + " " + I18n.INSTANCE.get("commonText.licence.upperCase") + " - " + textFieldName.getText());
+                                JOptionPane.showMessageDialog(null, I18n.INSTANCE.get("commonText.licence.upperCase") + ": [" + textFieldName.getText() + "] " + I18n.INSTANCE.get("commonText.successfullyAdded"), I18n.INSTANCE.get("textArea.added") + " " + getType(), JOptionPane.INFORMATION_MESSAGE);
                                 break;
                             } catch (IOException e) {
                                 e.printStackTrace();
-                                JOptionPane.showMessageDialog(null, "<html>Unable to add licence:<br>Exception: " + e.getMessage(), "Unable to add licence", JOptionPane.ERROR_MESSAGE);
+                                JOptionPane.showMessageDialog(null, "<html>" + I18n.INSTANCE.get("mod.licence.addMod.error") + ":<br>" + I18n.INSTANCE.get("commonBodies.exception") + ": " + e.getMessage(), I18n.INSTANCE.get("frame.title.error"), JOptionPane.ERROR_MESSAGE);
                             }
                         }
                     }else{
-                        JOptionPane.showMessageDialog(null, "Unable to add licence: The licence does already exist!", I18n.INSTANCE.get("frame.title.error"), JOptionPane.ERROR_MESSAGE);
+                        JOptionPane.showMessageDialog(null, I18n.INSTANCE.get("commonText.nameAlreadyInUse"), I18n.INSTANCE.get("frame.title.error"), JOptionPane.ERROR_MESSAGE);
                     }
                 }else{
-                    JOptionPane.showMessageDialog(null, "Please enter a licence name first!", "Unable to continue", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, I18n.INSTANCE.get("modManager.general.enterNameFirst"), I18n.INSTANCE.get("frame.title.error"), JOptionPane.ERROR_MESSAGE);
                 }
             }else{
                 break;
