93bde6901aecd3cb070362baa1e8d7942f12e492
Louis <louis.haensch01@gmail.com>
2022-11-23 23:38:22 +0100

    (#106) Small code cleanup

--- src/main/java/com/github/lmh01/mgt2mt/util/HelpSheetGenerator.java
+++ src/main/java/com/github/lmh01/mgt2mt/util/HelpSheetGenerator.java
@@ -165,20 +165,17 @@ public class HelpSheetGenerator {
             BufferedWriter bw = new BufferedWriter(new FileWriter(path.toFile()));
             bw.write(sb.toString());
             bw.close();
-            LOGGER.info("Cheat sheet has been generated successfully and is available here: " + path.toString());
-            TextAreaHelper.appendText(String.format(I18n.INSTANCE.get("textArea.helpSheetGenrated"), path.toString()));
+            LOGGER.info("Cheat sheet has been generated successfully and is available here: " + path);
+            TextAreaHelper.appendText(String.format(I18n.INSTANCE.get("textArea.helpSheetGenrated"), path));
             TextAreaHelper.appendText(I18n.INSTANCE.get("textArea.helpSheetPage"));
             TextAreaHelper.appendText(I18n.INSTANCE.get("textArea.helpSheetPage.copied"));
             Utils.copyToClipboard(sb.toString());
             if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                //Desktop.getDesktop().browse(new URI("file:///" + path));
                 Desktop.getDesktop().browse(path.toUri());
             }
         } catch (IOException e) {
             throw new ModProcessingException("Unable to generate cheat sheet", e);
-        } /*catch (URISyntaxException e) {
-            throw new ModProcessingException("Unable to open file in browser. Invalid URI.", e);
-        }*/
+        }
    } 
 
    /**
