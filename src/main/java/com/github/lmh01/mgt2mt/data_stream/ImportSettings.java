LMH01/MGT2_Mod_Tool

commit 25ed93d78ddd4bc043807f209844ec1e96acaaf8

Fixed #113 (untested, needs testing)

src/main/java/com/github/lmh01/mgt2mt/data_stream/ImportSettings.java
@@ -39,6 +39,7 @@ public class ImportSettings {
             Settings.enableExportStorage = toml.getBoolean("enableExportStorage");
             Settings.enableInitialBackupCheck = toml.getBoolean("enableInitialBackupCheck");
             Settings.writeTextAreaOutputToConsole = toml.getBoolean("writeTextAreaOutputToConsole");
+            Settings.maxPictureId = Integer.parseInt(toml.getString("maxPictureId"));
             Map<SafetyFeature, Boolean> safetyFeatures = new HashMap<>();
             for (SafetyFeature safetyFeature : SafetyFeature.values()) {
                 if (toml.getBoolean("safety_feature_" + safetyFeature.getIdentifier()) != null) {

src/main/java/com/github/lmh01/mgt2mt/MadGamesTycoon2ModTool.java
@@ -19,7 +19,7 @@ import java.util.Locale;
 public class MadGamesTycoon2ModTool {
     public static final OSType OS_TYPE;
     private static final Logger LOGGER = LoggerFactory.getLogger(MadGamesTycoon2ModTool.class);
-    public static final String VERSION = "4.7.0";//Version numbers that include "dev" are not checked for updates / tool will notify if update is available
+    public static final String VERSION = "4.7.1-dev";//Version numbers that include "dev" are not checked for updates / tool will notify if update is available
     public static final String CURRENT_RELEASE_VERSION = "4.7.0";//When this version number has been detected as the newest release version the update available message is held back
 
     static {

src/main/java/com/github/lmh01/mgt2mt/content/instances/Publisher.java
@@ -7,6 +7,7 @@ import com.github.lmh01.mgt2mt.content.manager.PublisherManager;
 import com.github.lmh01.mgt2mt.util.I18n;
 import com.github.lmh01.mgt2mt.util.Utils;
 import com.github.lmh01.mgt2mt.util.manager.TranslationManager;
+import com.github.lmh01.mgt2mt.util.settings.Settings;
 
 import java.io.File;
 import java.io.IOException;
@@ -132,7 +133,7 @@ public class Publisher extends AbstractAdvancedContent implements DependentConte
     @Override
     public void removePictures() throws IOException {
         //delete image only if it is not a standard game image
-        if (Integer.parseInt(icon.gameFile.getName().replaceAll("[^0-9]", "")) > 187) {
+        if (Integer.parseInt(icon.gameFile.getName().replaceAll("[^0-9]", "")) > Settings.maxPictureId) {
             Files.delete(icon.gameFile.toPath());
         }
     }

src/main/java/com/github/lmh01/mgt2mt/data_stream/ExportSettings.java
@@ -47,6 +47,7 @@ public class ExportSettings {
             map.put("enableExportStorage", Settings.enableExportStorage);
             map.put("enableInitialBackupCheck", Settings.enableInitialBackupCheck);
             map.put("writeTextAreaOutputToConsole", Settings.writeTextAreaOutputToConsole);
+            map.put("maxPictureId", Settings.maxPictureId);
             for (Map.Entry<SafetyFeature, Boolean> entry : Settings.safetyFeatures.entrySet()) {
                 map.put("safety_feature_" + entry.getKey().getIdentifier(), entry.getValue());
             }

src/main/java/com/github/lmh01/mgt2mt/util/settings/Settings.java
@@ -34,6 +34,7 @@ public class Settings {
     public static boolean enableExportStorage = true; //If true each new export will be saved in a new folder.
     public static boolean enableInitialBackupCheck = true; //If true the user will be notified if the initial backup is outdated
     public static boolean writeTextAreaOutputToConsole = false;
+    public static int maxPictureId = 196;
     public static Map<SafetyFeature, Boolean> safetyFeatures;
     public static String language = "English";
     public static UpdateBranch updateBranch = UpdateBranch.RELEASE;

commit ed2b59cbb69d2a9107b33a1612a89d1cb5fd8774

Fixed settings import that broke in 25ed93d78ddd4bc043807f209844ec1e96acaaf8

src/main/java/com/github/lmh01/mgt2mt/data_stream/ImportSettings.java
@@ -39,7 +39,6 @@ public class ImportSettings {
             Settings.enableExportStorage = toml.getBoolean("enableExportStorage");
             Settings.enableInitialBackupCheck = toml.getBoolean("enableInitialBackupCheck");
             Settings.writeTextAreaOutputToConsole = toml.getBoolean("writeTextAreaOutputToConsole");
-            Settings.maxPictureId = Integer.parseInt(toml.getString("maxPictureId"));
             Map<SafetyFeature, Boolean> safetyFeatures = new HashMap<>();
             for (SafetyFeature safetyFeature : SafetyFeature.values()) {
                 if (toml.getBoolean("safety_feature_" + safetyFeature.getIdentifier()) != null) {
@@ -50,6 +49,7 @@ public class ImportSettings {
                 }
             }
             Settings.safetyFeatures = safetyFeatures;
+            Settings.maxPictureId = Math.toIntExact(toml.getLong("maxPictureId"));
             return true;
         } catch (RuntimeException e) {
             LOGGER.info("Unable to import settings!:");