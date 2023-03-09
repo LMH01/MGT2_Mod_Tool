5a5a165fe07a84c304a92a5a57dd4173416251ac
LMH01 <louis.haensch01@gmail.com>
2023-03-09 20:47:23 +0100

    Changed version number to v4.9.0-beta7

--- src/main/java/com/github/lmh01/mgt2mt/content/manager/HardwareFeatureManager.java
+++ src/main/java/com/github/lmh01/mgt2mt/content/manager/HardwareFeatureManager.java
@@ -23,7 +23,7 @@ public class HardwareFeatureManager extends AbstractAdvancedContentManager {
 
     public static final HardwareFeatureManager INSTANCE = new HardwareFeatureManager();
 
-    public static final String[] compatibleModToolVersions = new String[]{"4.0.0", "4.1.0", "4.2.0", "4.2.1", "4.2.2", "4.3.0", "4.3.1", "4.4.0", "4.5.0", "4.6.0", "4.7.0", "4.8.0", "4.9.0-alpha1", "4.9.0-beta1",  "4.9.0-beta2",  "4.9.0-beta3", "4.9.0-beta4", "4.9.0-beta5", "4.9.0-beta6", MadGamesTycoon2ModTool.VERSION};
+    public static final String[] compatibleModToolVersions = new String[]{"4.0.0", "4.1.0", "4.2.0", "4.2.1", "4.2.2", "4.3.0", "4.3.1", "4.4.0", "4.5.0", "4.6.0", "4.7.0", "4.8.0", "4.9.0-alpha1", "4.9.0-beta1",  "4.9.0-beta2",  "4.9.0-beta3", "4.9.0-beta4", "4.9.0-beta5", "4.9.0-beta6", "4.9.0-beta7", MadGamesTycoon2ModTool.VERSION};
 
     private HardwareFeatureManager() {
         super("hardwareFeature", "hardware_feature", MGT2Paths.TEXT_DATA.getPath().resolve("HardwareFeatures.txt").toFile(), StandardCharsets.UTF_8);
