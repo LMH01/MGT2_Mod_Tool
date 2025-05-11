50ad0cc6178b9e3ffac4a5e45e43ef795991ef94
LMH01 <louis.haensch01@gmail.com>
2025-05-11 17:54:48 +0200

    updated version number to v5.0.0-beta1 for upcoming beta release

--- src/main/java/com/github/lmh01/mgt2mt/content/manager/GameplayFeatureManager.java
+++ src/main/java/com/github/lmh01/mgt2mt/content/manager/GameplayFeatureManager.java
@@ -23,7 +23,7 @@ public class GameplayFeatureManager extends AbstractAdvancedContentManager imple
 
     public static final GameplayFeatureManager INSTANCE = new GameplayFeatureManager();
 
-    public static final String[] compatibleModToolVersions = new String[]{"4.0.0", "4.1.0", "4.2.0", "4.2.1", "4.2.2", "4.3.0", "4.3.1", "4.4.0", "4.5.0", "4.6.0", "4.7.0", "4.8.0", "4.9.0-alpha1", "4.9.0-beta1",  "4.9.0-beta2",  "4.9.0-beta3", "4.9.0-beta4", "4.9.0-beta5", "4.9.0-beta6", "4.9.0-beta7", "4.9.0", "4.10.0", MadGamesTycoon2ModTool.VERSION};
+    public static final String[] compatibleModToolVersions = new String[]{"4.0.0", "4.1.0", "4.2.0", "4.2.1", "4.2.2", "4.3.0", "4.3.1", "4.4.0", "4.5.0", "4.6.0", "4.7.0", "4.8.0", "4.9.0-alpha1", "4.9.0-beta1",  "4.9.0-beta2",  "4.9.0-beta3", "4.9.0-beta4", "4.9.0-beta5", "4.9.0-beta6", "4.9.0-beta7", "4.9.0", "4.10.0", "5.0.0-beta1", MadGamesTycoon2ModTool.VERSION};
 
     private GameplayFeatureManager() {
         super("gameplayFeature", "gameplay_feature", MGT2Paths.TEXT_DATA.getPath().resolve("GameplayFeatures.txt").toFile(), StandardCharsets.UTF_8);
--- src/main/java/com/github/lmh01/mgt2mt/content/manager/ThemeManager.java
+++ src/main/java/com/github/lmh01/mgt2mt/content/manager/ThemeManager.java
@@ -31,7 +31,7 @@ public class ThemeManager extends AbstractSimpleContentManager implements Depend
 
     public static final ThemeManager INSTANCE = new ThemeManager();
 
-    public static final String[] compatibleModToolVersions = new String[]{"4.0.0", "4.1.0", "4.2.0", "4.2.1", "4.2.2", "4.3.0", "4.3.1", "4.4.0", "4.5.0", "4.6.0", "4.7.0", "4.8.0", "4.9.0-alpha1", "4.9.0-beta1",  "4.9.0-beta2",  "4.9.0-beta3", "4.9.0-beta4", "4.9.0-beta5", "4.9.0-beta6", "4.9.0-beta7", "4.9.0", "4.10.0", MadGamesTycoon2ModTool.VERSION};
+    public static final String[] compatibleModToolVersions = new String[]{"4.0.0", "4.1.0", "4.2.0", "4.2.1", "4.2.2", "4.3.0", "4.3.1", "4.4.0", "4.5.0", "4.6.0", "4.7.0", "4.8.0", "4.9.0-alpha1", "4.9.0-beta1",  "4.9.0-beta2",  "4.9.0-beta3", "4.9.0-beta4", "4.9.0-beta5", "4.9.0-beta6", "4.9.0-beta7", "4.9.0", "4.10.0", "5.0.0-beta1", MadGamesTycoon2ModTool.VERSION};
 
     private ThemeManager() {
         super("theme", "theme", MGT2Paths.TEXT.getPath().resolve("EN/Themes_EN.txt").toFile(), StandardCharsets.UTF_16LE);
